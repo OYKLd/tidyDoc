1b0469270546eb499ae77f64d9ffc14204cd979d
olydie <olydie.digifemmes@gmail.com>
2025-03-24 15:12:03 +0000

    aiArchive

--- tidydoc/src/firebase.js
+++ tidydoc/src/firebase.js
@@ -1,6 +1,8 @@
 // firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // Remplacez ces valeurs par celles de votre projet Firebase
 const firebaseConfig = {
@@ -18,4 +20,7 @@ const app = initializeApp(firebaseConfig);
 
 // Initialiser et exporter l'auth
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
 export default app;
