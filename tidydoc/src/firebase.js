d5e964516e7842e45486ab791ca607f182b1abee
olydie <olydie.digifemmes@gmail.com>
2025-03-24 14:40:40 +0000

    aiArchive

---
+++ tidydoc/src/firebase.js
@@ -0,0 +1,21 @@
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// Remplacez ces valeurs par celles de votre projet Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD77K9m19kHtfv5zrgvDIoOJfjA5iYg4Lw",
+  authDomain: "tidydoc.firebaseapp.com",
+  projectId: "tidydoc",
+  storageBucket: "tidydoc.firebasestorage.app",
+  messagingSenderId: "167012453317",
+  appId: "1:167012453317:web:7baf01fc2c62c26f3f59ac",
+  measurementId: "G-QLY5RYKH8L"
+};
+
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialiser et exporter l'auth
+export const auth = getAuth(app);
+export default app;
